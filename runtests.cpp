167dfb89e80df9fbec1f9292a0db4536377932c4
SteelMatrix <jpschool232@gmail.com>
2019-09-06 16:42:58 -0600

    Verified instructions and added additional funcitonality to runtests.cpp

--- runtests.cpp
+++ runtests.cpp
@@ -27,14 +27,41 @@ int main()
 	std::vector<int> search;
 	search.reserve(2000);
 
+	std::string confirm, file1, file2;
+
 	// TODO:
 	// read the file "numbers" into the numbers vector
 	// read the file "search" into the search vector
 	// since you're basically doing the same thing twice,
 	// write a function to read a single file into a vector
 	// and then call this function twice.
-	loadFile("numbers", numbers);
-	loadFile("search", search);
+
+	//User File Input
+	std::cout << "Use Default Files (Y/N)? ";
+	std::cin >> confirm;
+
+	if ((confirm == "Y") || (confirm == "y"))
+	{
+		loadFile("numbers", numbers);
+		loadFile("search", search);
+	}
+
+	else if ((confirm == "N") || (confirm == "n"))
+	{
+		std::cout << "\nEnter the file path for the number dataset to search: ";
+		std::cin >> file1;
+		std::cout << "\nEnter the file path for the search numbers: ";
+		std::cin >> file2;
+		std::cout << "Confirmed. Using custom files...\n\n";
+		loadFile(file1, numbers);
+		loadFile(file2, search);
+	}
+
+	else
+	{
+		std::cout << "\n\nIncorrect input. Please relaunch the program.";
+		return 0;
+	}
 
 	{
 		Timer timer("Time to linear search all values: ");
@@ -56,8 +83,7 @@ int main()
 		int found = 0;
 		for (size_t i = 0; i < search.size(); i++)
 		{
-			if (linearSearch(numbers.data(), numbers.data() + numbers.size(),
-					search[i]))
+			if (linearSearch(numbers.data(), numbers.data() + numbers.size(), search[i]))
 				found++;
 		}
 
